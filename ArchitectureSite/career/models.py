6b4fc80854c7f41584c37c306d0fc42ca68680ab
SamRocan <SamClendenan@iCloud.com>
2021-12-02 16:22:30 +0000

    Created models for career app

--- ArchitectureSite/career/models.py
+++ ArchitectureSite/career/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=10000)
+    requirements = models.CharField(max_length=10000)
+    salary = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class JobAppilcation(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    cover_letter = models.CharField(max_length=4000, blank=True, null=True)
+    cv = models.FileField(upload_to='CVs/', blank=True, null=True)
+    portfolio = models.FileField(upload_to='CVs/', blank=True, null=True)
