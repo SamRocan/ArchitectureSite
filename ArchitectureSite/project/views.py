79a66843df2a22511d952fcd4a067a8dd9052d34
SamRocan <SamClendenan@iCloud.com>
2021-12-02 15:38:57 +0000

    Basic Styling implemented for homepage

--- ArchitectureSite/project/views.py
+++ ArchitectureSite/project/views.py
@@ -6,4 +6,4 @@ def project_index(request):
     context ={
 
     }
-    return render(request, 'property/project_index.html', context)
+    return render(request, 'project/project_index.html', context)
