813aff3090f00101207938317a166b8eff3450d5
SamRocan <SamClendenan@iCloud.com>
2021-12-02 15:52:50 +0000

    Further Pages added

--- ArchitectureSite/main/views.py
+++ ArchitectureSite/main/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
+
+def about_us(request):
+    return render(request, 'main/about_us.html')
